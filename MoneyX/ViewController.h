2215ac21b769d00482b461b5f223ffcffbc115f1
AbdulKareem KhajaMohammed <abd.khaja@gmail.com>
2016-05-10 19:25:23 -0700

    Initial Commit

---
+++ MoneyX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoneyX
+//
+//  Created by Taiyaba Sultana on 5/10/16.
+//  Copyright © 2016 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MoneyXTests/MoneyXTests.m
@@ -0,0 +1,39 @@
+//
+//  MoneyXTests.m
+//  MoneyXTests
+//
+//  Created by Taiyaba Sultana on 5/10/16.
+//  Copyright © 2016 Abdul Kareem. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MoneyXTests : XCTestCase
+
+@end
+
+@implementation MoneyXTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
